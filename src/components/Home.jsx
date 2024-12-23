21ee58f24f1f737b51838bc5e3bf5e4f94684e8b
pkbtc <parasarkashyap06@gmail.com>
2024-12-23 13:08:35 +0530

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ function Home() {
   return (
     <div className="bg-black text-white font-mono leading-relaxed min-h-screen">
       {/* Social Links in Top-Right */}
-      <div className="absolute top-2 right-2 flex space-x-4">
+      <div className="absolute top-2 right-2 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 z-10">
         <a
           href="https://github.com/pkbtc"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:underline"
+          className="text-white hover:underline px-2 py-1"
         >
           GitHub
         </a>
@@ -17,21 +17,21 @@ function Home() {
           href="https://www.linkedin.com/in/parasar-kashyap-b06176240?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:underline"
+          className="text-white hover:underline px-2 py-1"
         >
           Linkedin
         </a>
         <a
           href="/resume.pdf"
           download
-          className="text-white hover:underline"
+          className="text-white hover:underline px-2 py-1"
         >
           Resume
         </a>
       </div>
 
       {/* Main Content */}
-      <div className="container mx-auto px-4 py-10 max-w-3xl scale-90 origin-top md:pl-0">
+      <div className="container mx-auto px-4 py-16 max-w-3xl scale-90 origin-top md:pl-0">
         <header>
           <h1 className="text-3xl font-bold mb-4 text-center md:text-left sm:whitespace-nowrap">
             hello, i am pk.eth
